0f888e53d4fa5374d3533d62feea608a6ef103c6
sridhar1011 <91914166+sridhar1011@users.noreply.github.com>
2026-02-27 20:45:43 +0530

    Add additional welcome message to main method

--- WelcomeToDevOps.java
+++ WelcomeToDevOps.java
@@ -1,5 +1,6 @@
 public class WelcomeToDevOps {
     public static void main(String[] args) {
         System.out.println("welcome to devops");
+        System.out.println("welcome to devops1");
     }
 }
